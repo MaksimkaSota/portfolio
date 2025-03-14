f9c7983799788a8403ea7fc64be39414eaa39140
Your Name <you@example.com>
2025-03-14 10:13:11 +0300

    feat: optimized portrait loading on the About page

--- src/Components/Main/About/About.tsx
+++ src/Components/Main/About/About.tsx
@@ -1,9 +1,27 @@
-import type { FC, ReactElement } from 'react';
+import { type FC, type ReactElement, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
+import cn from 'classnames';
 import classes from './About.module.scss';
-import portrait from '../../../assets/images/portrait.jpg';
-import CV from '../../../assets/documents/CV.pdf';
+import PortraitMin from '../../../assets/images/portrait.svg';
+import portraitMax from '../../../assets/images/portrait.jpg';
+import resume from '../../../assets/documents/resume.pdf';
 import { AltTxtKey, ContentTxtKey } from '../../../utils/types/enums';
+
+const Image: FC = (): ReactElement => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return (
+    <div className={classes.portraitContainer}>
+      <PortraitMin className={cn(classes.portrait, classes.portraitMin, { [classes.hiddenPortraitMin]: isLoaded })} />
+      <img
+        className={cn(classes.portrait, classes.portraitMax, { [classes.shownPortraitMax]: isLoaded })}
+        src={portraitMax}
+        alt={AltTxtKey.Photo}
+        onLoad={() => setIsLoaded(true)}
+      />
+    </div>
+  );
+};
 
 export const About: FC = (): ReactElement => {
   const { t } = useTranslation();
@@ -11,7 +29,7 @@ export const About: FC = (): ReactElement => {
   return (
     <main className={classes.aboutMe}>
       <div className={classes.wrapper}>
-        <img className={classes.portrait} src={portrait} alt={AltTxtKey.Photo} />
+        <Image />
         <div className={classes.content}>
           <h3 className={classes.title}>{t(ContentTxtKey.AboutTitle)}</h3>
           <h1 className={classes.mainTitle}>{t(ContentTxtKey.MainTitle)}</h1>
@@ -30,7 +48,7 @@ export const About: FC = (): ReactElement => {
               <p className={classes.education}>{t(ContentTxtKey.RSSText)}</p>
               <p className={classes.speciality}>{t(ContentTxtKey.SpecialityText3)}</p>
             </div>
-            <a className={classes.downloadButton} href={CV} download="Maksim Sotnikov">
+            <a className={classes.downloadButton} href={resume} download="Maksim Sotnikov">
               {t(ContentTxtKey.DownloadButton)}
             </a>
           </div>
